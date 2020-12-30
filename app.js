e074490e76259347b2746ed7212cb5f14ec4e0b2
developzego <developzego@gmail.com>
2020-12-30 09:58:35 +0700

    test

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.use(logger('dev'));
 app.use(express.json());
 
 app.post('/post',(req, res, next)=>{
-
-    res.json({result:"OK",body:req.body.data})
+    
+    res.json({result:"OK",body:req.body})
 })
 app.get('/',verify, (req, res, next) => {
     res.status(200).json({"result":"สวัสดี น้องแฮม this is api"})
